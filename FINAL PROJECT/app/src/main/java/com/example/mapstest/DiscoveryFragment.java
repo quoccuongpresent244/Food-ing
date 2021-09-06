fbfe893e24336ffc28d71601c985a646e3d26667
Quoc Nguyen <quoccuongpresent@gmail.com>
2021-09-06 17:15:27 +0700

    update discovery

--- FINAL PROJECT/app/src/main/java/com/example/mapstest/DiscoveryFragment.java
+++ FINAL PROJECT/app/src/main/java/com/example/mapstest/DiscoveryFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class DiscoveryFragment extends Fragment {
 
@@ -19,6 +20,16 @@ public class DiscoveryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discovery, container, false);
+        View view = inflater.inflate(R.layout.fragment_discovery, container, false);
+        ImageView backToMapBtn = view.findViewById(R.id.backToMapBtn);
+        backToMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+        return view;
     }
+
+
 }
